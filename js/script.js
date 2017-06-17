abec0420139ee071ae6a16edf3c449851255e59f
kervin5 <kervinlvh@gmail.com>
2017-06-17 14:09:47 -0700

    Create channels basic layout

--- js/script.js
+++ js/script.js
@@ -1,12 +1,16 @@
 //Javascript
 //channel endpoint https://api.twitch.tv/kraken/channels/
 //stream endpoint https://api.twitch.tv/kraken/streams/
-var nickname = "streamerhouse";
+var channels = ["streamerhouse,syndicate"];
 
-function CheckOnlineStatus()
+function displayChannel(channel) {
+
+}
+
+function checkOnlineStatus(channel)
 {
     $.ajax({
-        url: "https://api.twitch.tv/kraken/channels/"+nickname,
+        url: "https://api.twitch.tv/kraken/channels/"+channel,
         dataType: 'json',
         headers: {
             'Client-ID': "7buzh4k369ta1n0se34wbn8aj2b7jr"
@@ -15,12 +19,19 @@ function CheckOnlineStatus()
         console.log(channel);
         if (channel["stream"] === null)
         {
-            alert(nickname+" is not online");
+            alert(channel+" is not online");
+            $("#channels");
         } else {
-            alert(nickname+" is online!");
+            //alert(nickname+" is online!");
         }
     }).fail(function (data) {
         console.log("Error",data);
     });
 }
-CheckOnlineStatus();
+
+
+function getChannels(channels) {
+    for(var i = 0; i < channels.length; i++){
+        checkOnlineStatus(channels[i]);
+    }
+}
