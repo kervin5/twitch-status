67d72ba6b552f8a6a09ded2de18b0a91da540da5
kervin5 <kervinlvh@gmail.com>
2017-06-17 17:13:02 -0700

    Add test channels

--- js/script.js
+++ js/script.js
@@ -1,16 +1,20 @@
 //Javascript
 //channel endpoint https://api.twitch.tv/kraken/channels/
 //stream endpoint https://api.twitch.tv/kraken/streams/
-var channels = ["streamerhouse,syndicate"];
+var channels = ["streamerhouse","riotgames","summit1g","esltv_cs","nightblue3","imaqtpie", "OgamingSC2", "cretetion", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas"];
 
-function displayChannel(channel) {
-
+function displayChannel(channelRes,channelName) {
+    var channelInfo = "<div></div>";
+    var channelRow = " <div class='col-xs-6 col-sm-3 col-md-2'> <div class='channel-container' id='%id%'><img src='%img%' alt='logo' class='img-responsive'/></div> </div>";
+    channelRow = channelRow.replace("%img%",channelRes.logo);
+    channelRow = channelRow.replace("%id%",channelName);
+    $("#channels").append(channelRow);
 }
 
-function checkOnlineStatus(channel)
+function checkOnlineStatus(channelName)
 {
     $.ajax({
-        url: "https://api.twitch.tv/kraken/channels/"+channel,
+        url: "https://api.twitch.tv/kraken/channels/"+channelName,
         dataType: 'json',
         headers: {
             'Client-ID': "7buzh4k369ta1n0se34wbn8aj2b7jr"
@@ -20,9 +24,9 @@ function checkOnlineStatus(channel)
         if (channel["stream"] === null)
         {
             alert(channel+" is not online");
-            $("#channels");
         } else {
-            //alert(nickname+" is online!");
+            //alert(channel+" is online!");
+            displayChannel(channel,channelName);
         }
     }).fail(function (data) {
         console.log("Error",data);
@@ -35,3 +39,5 @@ function getChannels(channels) {
         checkOnlineStatus(channels[i]);
     }
 }
+
+getChannels(channels);
